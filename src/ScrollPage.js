6f16789bc0c9e929f5611abfdecff0c4d4b80dd5
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-05-14 12:17:35 +0200

    Patch[1.2.14]

--- src/ScrollPage.js
+++ src/ScrollPage.js
@@ -1,13 +1,17 @@
+import { useState } from "react";
 
 export default function ScrollPage() {
+  const [height, setHeight] = useState(window.innerHeight);
+
+    window.addEventListener("resize", ()=>setHeight(window.innerHeight))
 
   return (
     <div className="flex flex-col flex-1 justify-between">
-      <div className="bg-white overflow-y-scroll text-9xl h-full">
-         
+      <div className="bg-white overflow-y-scroll text-9xl h-full" style={{ height: `calc(${height}px - 129px)` }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, esse velit vitae ipsa deserunt praesentium blanditiis molestiae repudiandae voluptates et consequuntur soluta est reprehenderit ipsam eum molestias tempora repellendus quas.
       </div>
       <div className="flex m-2">
-        <input type="text" className="w-full border-2 border-black"  />
+        <input type="text" className="w-full border-2 border-black" />
         <img src={require("./img/send.png")} alt="" width="40" className="ml-2" />
       </div>
     </div>
