cc54426b9d16eeac347fdbad71e6e304d212dec1
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-05-14 11:21:38 +0200

    Patch[1.2.9]

--- src/ScrollPage.js
+++ src/ScrollPage.js
@@ -1,6 +1,13 @@
 export default function ScrollPage() {
   return (
     <div className="flex flex-col bg-red-300 h-full">
+        <div className="flex-1">
+
+        </div>
+        <div className="flex m-2">
+            <input type="text" className="w-full"/>
+            <img src={require("./img/send.png")} alt="" width="40" className="ml-2"/>
+        </div>
     </div>
   );
 }
