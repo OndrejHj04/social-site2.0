dc79d6e2d4a6387e047479b7a4e035876e43bac5
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-05-14 12:06:28 +0200

    Patch[1.2.13]

--- src/ScrollPage.js
+++ src/ScrollPage.js
@@ -1,13 +1,15 @@
-export default function ScrollPage() {
-  return (
-    <div className="flex flex-col h-full">
-        <div className="bg-white overflow-y-scroll h-full">
 
-        </div>
-        <div className="flex m-2">
-            <input type="text" className="w-full border-2 border-black"/>
-            <img src={require("./img/send.png")} alt="" width="40" className="ml-2"/>
-        </div>
+export default function ScrollPage() {
+
+  return (
+    <div className="flex flex-col flex-1 justify-between">
+      <div className="bg-white overflow-y-scroll text-9xl h-full">
+         
+      </div>
+      <div className="flex m-2">
+        <input type="text" className="w-full border-2 border-black"  />
+        <img src={require("./img/send.png")} alt="" width="40" className="ml-2" />
+      </div>
     </div>
   );
 }
