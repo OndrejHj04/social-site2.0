9d4114e22b74ebb6339a84e7e54e3829788b8c8d
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-05-17 20:16:52 +0200

    Patch[1.8]
    Now I am finally satisfied with my work 😎

--- src/Message.js
+++ src/Message.js
@@ -8,13 +8,15 @@ export default function Message({ item, remove, activeUser, name }) {
 
     return `${day}. ${month}. ${hours}:${minutes}`;
   };
+
+  console.log(item.user, activeUser.username)
   return (
-    <div className=" w-full break-words my-1 group ">
-      {name && <h1 className={`${activeUser.username === item.user&&"text-right"}`}>{name}</h1>}
+    <div className=" w-full break-words group ">
+      {name && <h1 className={`${activeUser.username === item.user&&"text-right"}`}>{item.user === activeUser.username?"you":name}</h1>}
       <div className={`${activeUser.username === item.user&&"justify-end"} break-words flex`}>
-        <h1 className={`bg-blue text-white p-2 rounded-3xl ${activeUser.username === item.user&&"order-2"} ${activeUser.username === item.user?"rounded-tr-none":"rounded-tl-none"}`} style={{maxWidth: "80%"}} >{item.text}</h1>
-        <div className="flex mb-auto text-lg my-auto flex-wrap mx-1">
-          <img src={require("./img/trash.png")} alt="" width="30" className={`group-hover:scale-y-100 m-auto scale-y-0 transition-all ${activeUser.username !== item.user&&"hidden"}`} onClick={() => remove(item.id, item.user)}/>
+        <h1 className={`bg-blue text-white my-1 p-2 mb-auto rounded-3xl ${activeUser.username === item.user&&"order-2"} ${activeUser.username === item.user?"rounded-tr-none":"rounded-tl-none"}`} style={{maxWidth: "80%"}} >{item.text}</h1>
+        <div className="flex mb-auto my-auto flex-wrap mx-1">
+          <img src={require("./img/trash.png")} alt="" width="25" className={`group-hover:scale-y-100 m-auto scale-y-0 transition-all ${activeUser.username !== item.user&&"hidden"}`} onClick={() => remove(item.id, item.user)}/>
           <p className="my-auto group-hover:scale-x-100 scale-x-0 transition-all text-center">{milis()}</p>
         </div>
       </div>
@@ -22,17 +24,4 @@ export default function Message({ item, remove, activeUser, name }) {
   );
 }
 
-// <div className={` flex group ${activeUser.username === item.user&&"justify-end"}`} id={item.time}>
-//   <div className={`flex flex-col ${activeUser.username === item.user&&"order-2"}`}>
-//     {name&&<div className={`flex justify-between ${activeUser.username === item.user&&"ml-auto"}`} >
-//       <h1 >{activeUser.username === name?"You":name}</h1>
-//     </div>}
-//     <div className={`bg-blue text-white p-2 text-lg w-fit rounded-2xl ${activeUser.username === item.user?"ml-auto rounded-br-none": "rounded-bl-none"} ${name?"mb-1":"my-1"}`}>
-//       <p className="">{item.text}</p>
-//     </div>
-//   </div>
-//   <div className={`flex flex-col ${activeUser.username === item.user?"order-1 mr-3":"ml-3"}`}>
-//     <p className="scale-x-0 group-hover:scale-x-100 transition-all text-center">{milis()}</p>
-//     <img src={require("./img/trash.png")} alt="" width="30" className={`m-auto scale-y-0 group-hover:scale-y-100 transition-all ${activeUser.username !== item.user&&"invisible"}`} onClick={() => remove(item.id, item.user)}/>
-//   </div>
-// </div>
+
--- src/Nav.js
+++ src/Nav.js
@@ -10,12 +10,12 @@ export default function Nav({ url, activeUser, changeActiveUser }) {
   
   return (
     <>
-      <div className="bg-blue p-3 flex">
+      <div className="bg-blue p-3 flex justify-between">
         <h1 className="font-chalk text-5xl text-white">Blig</h1>
         {url.includes("scroll-page") && activeUser && activeUser.username && (
           <>
-            <h1 className="my-auto mx-2">{activeUser.username}</h1>
-            <h1 className="cursor-pointer my-auto mx-2 ml-auto" onClick={logOut}>Log out</h1>
+            <h1 className="my-auto mx-2 text-2xl text-white">{activeUser.username}</h1>
+            <img alt="" width="25" src={require("./img/exit.png")} className="cursor-pointer my-auto text-xl" onClick={logOut} />
           </>
         )}
       </div>
