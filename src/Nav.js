fc9b1a4857e8926e472e8aed0f60f3c954a35de9
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-05-23 07:42:34 +0200

    Patch[3.0.1]
    Last dance with dev page

--- src/Nav.js
+++ src/Nav.js
@@ -1,21 +1,24 @@
 import { Outlet, useNavigate } from "react-router-dom";
 
 export default function Nav({ url, activeUser, changeActiveUser }) {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const logOut = () => {
-    changeActiveUser()
-    navigate("/login")
-  }
-  
+    changeActiveUser();
+    navigate("/login");
+  };
+
   return (
     <>
       <div className="bg-blue p-3 flex justify-between">
-        <h1 className="font-chalk text-5xl text-white cursor-pointer" onClick={()=>navigate("/development")}>Blig</h1>
+        <h1 className="font-chalk text-5xl text-white cursor-pointer" onClick={() => url.includes("development")?navigate(-1):navigate("/development")}>
+          Blig
+        </h1>
         {url.includes("scroll-page") && activeUser && activeUser.username && (
           <>
-            <h1 className="my-auto mx-2 text-2xl text-white flex-1 text-center" style={{fontSize: "min(5vw, 30px)"}}>{activeUser.username}</h1>
+            <h1 className="my-auto mx-2 text-2xl text-white flex-1 text-center" style={{ fontSize: "min(5vw, 30px)" }}>
+              {activeUser.username}
+            </h1>
             <img alt="" width="25" src={require("./img/exit.png")} className="cursor-pointer my-auto text-xl" onClick={logOut} />
           </>
         )}
--- src/Patch.js
+++ src/Patch.js
@@ -6,7 +6,7 @@ export default function Patch({ item }) {
     <>
       <div className="flex justify-between text-3xl my-1" onClick={() => setShow(!show)}>
         <h1 className="my-auto">{`${item.me.commit.committer.date.substring(8, 10)}. ${item.me.commit.committer.date.substring(5, 7)}.`}</h1>
-        <h1 className="my-auto">{item.me.commit.message.split("\n\n")[0]}</h1>
+        <h1 className="my-auto">{item.me.commit.message.split("\n\n")[0].replace("Patch", "Version")}</h1>
         <img src={require("./img/more.png")} alt="" width="55"  className={`transition-all ${show ? "rotate-180" : "rotate-0"} ${!item.children.length&&"scale-0"}`} />
       </div>
       <hr />
